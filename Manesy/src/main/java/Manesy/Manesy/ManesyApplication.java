62cc7c7515101e2875b6894ec62bec867578299f
claudio <portoclaudio85@gmail.com>
2023-10-30 21:21:42 -0300

    first commit

---
+++ Manesy/src/main/java/Manesy/Manesy/ManesyApplication.java
@@ -0,0 +1,13 @@
+package Manesy.Manesy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManesyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManesyApplication.class, args);
+	}
+
+}
---
+++ Manesy/src/test/java/Manesy/Manesy/ManesyApplicationTests.java
@@ -0,0 +1,13 @@
+package Manesy.Manesy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManesyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
